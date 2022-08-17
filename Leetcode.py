753e7a8031769185b3967bd784ed5b137e6931c2
Asilbek <47601374+yaxshiboyev@users.noreply.github.com>
2022-08-17 14:17:56 +0200

    Update Leetcode.py

--- Leetcode.py
+++ Leetcode.py
@@ -1,4 +1,6 @@
-class Solution:
+#Roman to Integer Leetcode 13
+"""
+class Solution:   
     def romanToInt(self, s: str) -> int:
 
     lookup = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
@@ -17,3 +19,4 @@ ineger= Solution()
 
 
 print(ineger.romanToInt(roman))
+"""
